ed28485b971f90d42c63bd9d834fc86702496ce0
Carlos Schuenck <carlosschuenck@hotmail.com>
2019-05-02 01:10:23 -0300

    Adicionando método para buscar todos os boxes.

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,18 +1,23 @@
-const Box = require('../models/Box');
+const Box = require("../models/Box");
 
 class BoxController {
-    async store(req, res){
-        const box = await Box.create(req.body);
-        return res.json(box);
-    }
+  async store(req, res) {
+    const box = await Box.create(req.body);
+    return res.json(box);
+  }
 
-    async show(req, res){
-        const box = await Box.findById(req.params.id).populate({
-            path: 'files',
-            options: { sort: { createdAt: -1}}
-        });
-        return res.json(box);
-    }
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: "files",
+      options: { sort: { createdAt: -1 } }
+    });
+    return res.json(box);
+  }
+
+  async findAll(req, res) {
+    const boxes = await Box.find();
+    return res.json(boxes);
+  }
 }
 
 module.exports = new BoxController();
